2b145dc629fbd8d4980397816dda8dcba58d78b2
Minh <minhtth5@fpt.edu.vn>
2025-12-07 15:57:14 +0700

    Add Food List

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -8,6 +8,7 @@ import {
 } from "react-native-responsive-screen";
 import Categories from "../components/categories";
 import FoodItems from "../components/recipes";
+import Recipe from "../components/recipes";
 
 export default function HomeScreen() {
   const [activeCategory, setActiveCategory] = useState("Chicken");
@@ -737,7 +738,7 @@ export default function HomeScreen() {
         </View>
 
         <View testID="foodList">
-
+          <Recipe foods={allFood} categories={categories} />
         </View>
       </ScrollView>
     </View>
